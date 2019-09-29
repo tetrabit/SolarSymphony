caeb5aae41aaaec4329f0edcbc5d74a33c1ed08d
Joshua Nieves <joshuanieves08@gmail.com>
2019-09-29 12:16:15 -0400

    Added toggle script to hide images

---
+++ SolarSymphony/Assets/SolarSymphony/Joshua/Scripts/JN_SampleSounds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JN_SampleSounds : MonoBehaviour
+{
+    private void Start()
+    {
+        //GetComponent<Toggle>().graphic = transform.GetChild(0).GetComponent<Image>();
+    }
+}
