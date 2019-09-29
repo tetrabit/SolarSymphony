4f9801aeb605ec1da0c69183ff14787a5d40a744
Benjamin Zeng <benjamin.zeng89@gmail.com>
2019-09-29 11:55:04 -0400

    Added/Mod 3 Lighting Planet Scripts

--- SolarSymphony/Assets/SolarSymphony/Scripts/PlanetHaloControl.cs
+++ SolarSymphony/Assets/SolarSymphony/Scripts/PlanetHaloControl.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlanetHaloControl : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] PlanetLight;
 
+    [SerializeField]
+    private PlanetLight[] _planetLights;
+
     private int numPlanets;
+    //private Halo[] PlanetHalo;
+    //[SerializeField] private bool _isEnabled = true;
+    //[SerializeField] private Color _color = Color.red;
+    [SerializeField] private float _size = 1;
+
+    public PlanetLight[] PlanetLights { get => _planetLights; set => _planetLights = value; }
+
     void Start()
     {
-        numPlanets = PlanetLight.Length;
+        numPlanets = PlanetLights.Length;
+        // x = PlanetLight[numPlanets].GetComponent("Halo");
+        //PlanetLight[numPlanets].GetComponent("Halo"). = false;
+        for(int x = 0; x < PlanetLights.Length; x++ )
+        {
+            SerializedObject halo = new SerializedObject(PlanetLight[x].GetComponent("Halo"));
+            halo.FindProperty("m_Size").floatValue =100;
+            //halo.FindProperty("m_Enabled").boolValue = _isEnabled;
+            //halo.FindProperty("m_Color").colorValue = _color;
+            halo.ApplyModifiedProperties();
+
+
+
+            //PlanetLight[numPlanets].GetComponent<Light>().GetComponent<Halo>().getPr
+
+            //Component halo = PlanetLight[x].GetComponent("Halo");
+            //halo.GetType().GetProperty("Size").SetValue(halo, 1000);
+            //Debug.Log(halo.GetType());
+            //Debug.Log(halo.GetType().GetProperty("Size"));
+            //SolarSymphony.Instance.SolarSymphonyEvents.PlanetTrigger(Planet.Earth);
+        }
+        
+
 
     }
 
@@ -18,5 +51,22 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void TriggerHaloEffect(Planet planet)
+    {
+        for (int i = 0; i < PlanetLights.Length; i++)
+        {
+            if(PlanetLights[i].Planet == planet)
+            {
+                //make light effect happen here
+            }
+
+        }
+    }
+    
+    void PlayPlanetSound(int planet)
+    {
+
     }
 }
