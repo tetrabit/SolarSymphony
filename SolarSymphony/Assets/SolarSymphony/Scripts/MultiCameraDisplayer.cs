35276a8e98545b43c13146ea8b5acc2ce4a3bc5e
Shawn De La Cruz <sdelacruz@fullsail.edu>
2019-09-29 12:27:39 -0400

    Prepush for MultiCameraDisplayer implementation

---
+++ SolarSymphony/Assets/SolarSymphony/Scripts/MultiCameraDisplayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiCameraDisplayer : MonoBehaviour
+{
+    Camera[] myCams = new Camera[2];
+    void Start()
+    {
+        //Get Main Camera
+        myCams[0] = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+       
+        //Find All other Cameras
+        myCams[1] = GameObject.Find("Camera2").GetComponent<Camera>();
+
+        //Call function when new display is connected
+        Display.onDisplaysUpdated += OnDisplaysUpdated;
+
+        //Map each Camera to a Display
+        mapCameraToDisplay();
+    }
+
+    void mapCameraToDisplay()
+    {
+        //Loop over Connected Displays
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            myCams[i].targetDisplay = i; //Set the Display in which to render the camera to
+            Display.displays[i].Activate(); //Enable the display
+        }
+    }
+
+    void OnDisplaysUpdated()
+    {
+        Debug.Log("New Display Connected. Show Display Option Menu....");
+    }
+}
